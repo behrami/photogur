bda1fb4b83eed8daf3a5feca400e3ef2ff8b8cae
behrami <behram1993@gmail.com>
2018-04-22 13:29:45 -0400

    added validation for new picture and new user

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,11 @@
 class Picture < ApplicationRecord
+  validates :title, presence: true
+
+  validates :artist, presence: true
+  validates :artist, length: {minimum: 3}
+  validates :artist, length: {maximum: 20}
+
+  validates :url, presence: true
+  validates :url, uniqueness: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+  
   validates :email, presence: true
+
+  validates :password, presence: true
+
+  validates :password_confirmation, presence: true
 end
