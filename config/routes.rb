2e17ed1d24db089844b28b201784f7227acf72b9
behrami <behram1993@gmail.com>
2018-04-22 12:37:34 -0400

    added login

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,12 @@
 Rails.application.routes.draw do
   root 'pictures#index'
   get 'pictures' => 'pictures#index'
-  
-  get 'pictures' => 'pictures#index'
 
   post 'pictures' => 'pictures#create'
   get 'pictures/new' => 'pictures#new'
 
   get 'pictures/:id' => 'pictures#show'
+
+  resource :users
+  resource :sessions
 end
